c62e475d1dec7a064db219a4f85f925040eed344
guokangxun <guokangxun@163.com>
2018-12-15 00:33:45 +0800

    添加原生js选中底部导航icon

--- static/js/fontsize.js
+++ static/js/fontsize.js
@@ -9,3 +9,13 @@ window.addEventListener('resize',function(){
 		htmlDom.style.fontSize = screenWidth / 10 + 'px';
 		console.log(htmlDom.style.fontSize);
 });
+
+var uli =document.querySelectorAll(".rem-nav li");
+for(var i=0, len=uli.length; i< len; i++){
+	uli[i].onclick = function(){
+		for (var i = 0; i < len; i++) {
+			uli[i].className = '';
+		}
+		this.className= "active";
+	}
+}
